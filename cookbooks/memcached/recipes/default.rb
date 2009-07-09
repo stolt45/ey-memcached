503d3cf2c48fc3f40f2d0ef080146924ce058801
Christopher Stolt <stolt45@christopher-stolts-macbook-pro.local>
2009-07-09 12:28:17 -0600

    adjusting template

--- cookbooks/memcached/recipes/default.rb
+++ cookbooks/memcached/recipes/default.rb
@@ -1,6 +1,6 @@
 require 'pp'
 #
-# Cookbook Name:: databaseyml
+# Cookbook Name:: memcached
 # Recipe:: default
 #
 
@@ -8,14 +8,14 @@ node[:applications].each do |app_name,data|
   user = node[:users].first
   db_name = "#{app_name}_#{node[:environment][:framework_env]}"
   
-  template "/data/#{app_name}/shared/config/database.yml" do
-    source "database.yml.erb"
+  template "/data/#{app_name}/shared/config/memcached.yml" do
+    source "memcached.yml.erb"
     owner user[:username]
     group user[:username]
     mode 0744
     variables({
         :app_name => app_name,
-        :db_pass => user[:password]
+        :servers => node[:members]
     })
   end
 end
