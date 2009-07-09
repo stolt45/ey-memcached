03ead874b3885173510910f03b17e6713f005d69
Christopher Stolt <stolt45@christopher-stolts-macbook-pro.local>
2009-07-09 15:22:46 -0600

    adding memcached.erb and a case to exclude the db slice

--- cookbooks/memcached/recipes/default.rb
+++ cookbooks/memcached/recipes/default.rb
@@ -7,7 +7,9 @@ require 'pp'
 node[:applications].each do |app_name,data|
   user = node[:users].first
   db_name = "#{app_name}_#{node[:environment][:framework_env]}"
-  
+
+case node[:instance_roll]
+when "app"
   template "/data/#{app_name}/shared/config/memcached.yml" do
     source "memcached.yml.erb"
     owner user[:username]
@@ -18,4 +20,14 @@ node[:applications].each do |app_name,data|
         :servers => node[:members]
     })
   end
+
+  template "/etc/conf.d/memcached" do
+    owner 'root'
+    group 'root'
+    mode 0644
+    source "memcached.erb"
+    variables :memusage => node[:memcached_size] || 512,
+              :port     => node[:port] || 11211
+  end
+end
 end
